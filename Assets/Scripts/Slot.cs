39ad44ef6e13de8fb0127d62202c8bee3d0b9be7
choihabin <chabin7181@gmail.com>
2025-05-15 00:53:22 +0900

    feat : item data & scriptable object 생성

--- Assets/Scripts/Slot.cs
+++ Assets/Scripts/Slot.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class Slot : MonoBehaviour
 {
     public Vector2Int gridPos; // (x,y) 위치
-    
+    public Item currentItem;
+
+    public bool isEmpty => currentItem == null;
+
     // Start is called before the first frame update
     void Start()
     {
