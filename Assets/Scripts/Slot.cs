067b3abf675f9b350797661c4a693e9c17e3b4de
choihabin <chabin7181@gmail.com>
2025-05-27 22:37:50 +0900

    update: 사용하지 않는 함수, 네임스페이스 삭제

--- Assets/Scripts/Slot.cs
+++ Assets/Scripts/Slot.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Slot : MonoBehaviour
@@ -8,17 +6,4 @@ public class Slot : MonoBehaviour
     public Item currentItem;
 
     public bool isEmpty => currentItem == null;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
 }
